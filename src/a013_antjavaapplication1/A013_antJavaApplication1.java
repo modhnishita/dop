b6fc186bf2e1e3b04eb2b8464b2ae39893593581
modhnishita <ngre8t@gmail.com>
2025-12-11 08:06:04 +0530

    java ant

---
+++ src/a013_antjavaapplication1/A013_antJavaApplication1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package a013_antjavaapplication1;
+
+/**
+ *
+ * @author NMCC STUDENT
+ */
+public class A013_antJavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
